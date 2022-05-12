5851a8b1102aa813afcbb1f9bc357a24fb52cbc7
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:31 -0300

    chore: re index ffbookmarks

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."ffbookmarks-master".dir   = "master";
+  inputs."ffbookmarks-master".owner = "nim-nix-pkgs";
+  inputs."ffbookmarks-master".ref   = "master";
+  inputs."ffbookmarks-master".repo  = "ffbookmarks";
+  inputs."ffbookmarks-master".type  = "github";
+  inputs."ffbookmarks-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ffbookmarks-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
